3ba10cef6338e90ad1227b9878ca9e8049debbca
Ben Wigley <benwigley2@gmail.com>
2013-04-28 19:13:22 +1200

    Button select almost complete.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+jQuery(document).ready(function($) {
 
+
+	// Button select
+	// -------------
+
+	$('select.button-select.custom').buttonSelect({
+		'buttonClass': 'button'
+	});
+
+	$('select.button-select.bootstrap').buttonSelect({
+		'buttonClass': 'btn'
+	});
+
+
+	// Scrollbrakes
+	// -------------
+
+	// $('.scrollbrakes').scrollbrakes();
+
+
+	// Input resizer
+	// -------------
+
+	// $('.input-rezieable').inputResizer();
+
+});
